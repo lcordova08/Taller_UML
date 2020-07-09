bd44887e5a1d472c7df043ca75afa3685a3d95d8
Ronny-Andrade <66749405+Ronny-Andrade@users.noreply.github.com>
2020-07-09 11:06:54 -0500

    Clase secretaria
    Secretaria se agrega a Doctor

---
+++ src/Sistema_Clinico/Secretaria.java
@@ -0,0 +1,17 @@
+package Sistema_Clinico;
+
+import java.util.Date;
+
+public class Secretaria extends Persona{
+	Doctor  doSec;
+	Secretaria(Doctor doSec, String usuario, String clave, String nombre, String apellido, String cedula, String direccion, Date fechaNac) {
+		super(usuario, clave, nombre, apellido, cedula, direccion, fechaNac);
+		this.doSec = doSec;
+	}
+	public void verificarCita() {
+		
+	}
+	public void agendarCita() {
+		
+	}
+}
